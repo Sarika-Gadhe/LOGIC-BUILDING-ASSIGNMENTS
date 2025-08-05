e2c62983417059e20d3f3f43e3c90f2b257840b1
Sarika Devidas Gadhe <sarikagadhe60@gmail.com>
2025-08-06 02:28:08 +0530

    Add files via upload

---
+++ Assignment_11/Program3.c
@@ -0,0 +1,35 @@
+//_______________________________________________________________________________________________________________________________
+//
+//  File name :     Program3.c
+//  Description :   Accept number from user and display below pattern.
+//
+//                          Input :  5        
+//                          Output : 1 * 2 * 3 * 4 * 5 *
+//
+//  Author :        Sarika Devidas Gadhe
+//  Date :          20/05/2025
+//
+//_______________________________________________________________________________________________________________________________
+
+#include<stdio.h>
+
+void Pattern (int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("%d\t*\t",iCnt);
+    }
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number of elements ;\n");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
